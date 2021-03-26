4a367e4dc8066ac0ce5f2b08a008f81a8890b5e1
summerpray <1141430447@qq.com>
2021-03-26 19:01:06 +0800

    add Parse web pages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import investpy
+import time
 import requests
+import re
 
 """请求网页"""
 
@@ -13,3 +14,11 @@ response = requests.get('https://cn.investing.com/commodities/crude-oil',headers
 html = response.text
 
 """解析网页"""
+oil_price = re.findall('<span class="arial_26 inlineblock pid-8849-last" id="last_last" dir="ltr">(.*?)</span>', html)
+change_price = re.findall('<span class="arial_20 greenFont   pid-8849-pc" dir="ltr">(.*?)</span>', html)
+change_persent = re.findall('<span class="arial_20 greenFont  pid-8849-pcp parentheses" dir="ltr">(.*?)</span>', html)
+print(oil_price)
+print(change_price)
+print(change_persent)
+
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
