f71e4444e03b72e2238827b877fd22bf85423939
summerpray <1141430447@qq.com>
2021-03-26 21:14:43 +0800

    add all the functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import csv
 import re
 
 """请求网页"""
@@ -9,16 +10,40 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
 }
 
-response = requests.get('https://cn.investing.com/commodities/crude-oil',headers=headers)
+response = requests.get('https://cn.investing.com/commodities/crude-oil', headers=headers)
 # print(response.request.headers)
 html = response.text
 
 """解析网页"""
 oil_price = re.findall('<span class="arial_26 inlineblock pid-8849-last" id="last_last" dir="ltr">(.*?)</span>', html)
-change_price = re.findall('<span class="arial_20 greenFont   pid-8849-pc" dir="ltr">(.*?)</span>', html)
-change_persent = re.findall('<span class="arial_20 greenFont  pid-8849-pcp parentheses" dir="ltr">(.*?)</span>', html)
-print(oil_price)
-print(change_price)
-print(change_persent)
+"""执行脚本"""
+month = ''
+day = ''
 
-print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+while(1):
+    response = requests.get('https://cn.investing.com/commodities/crude-oil', headers=headers)
+    html = response.text
+    month_last = month
+    day_last = day
+    now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    month = now[5:7]
+    day = now[8:10]
+    filename = now[0:4] + "Y" + month + "M" + day + "D" + "oilprice" + ".csv"
+    if (day != day_last or month != month_last):
+        with open(filename, 'a') as csvfile:
+            csv_write = csv.writer(csvfile)
+            csv_head = ["oil_price", "time"]
+            csv_write.(csv_head)
+    else:
+        oil_price_last = oil_price
+        oil_price = str(re.findall('id="last_last" dir="ltr">(.*?)</span>', html))
+        time_temp = now
+        list = [oil_price, time_temp]
+        # if (oil_price_last != oil_price):
+        with open(filename, 'a', newline='') as csvfile:
+            csv_write = csv.writer(csvfile)
+            print(oil_price)
+            csv_write.writerow(list)
+    time.sleep(60)
+# print(day)
+
